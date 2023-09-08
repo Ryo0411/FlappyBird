3af5beb8c92502a473119d1a5939ed80963b024a
ryo-c-watanabe <ryo.c.watanabe@dena.com>
2023-09-08 11:52:38 +0900

    Initial Commit

---
+++ FlappyBird/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FlappyBird
+//
+//  Created by Ryo Watanabe on 2023/09/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
